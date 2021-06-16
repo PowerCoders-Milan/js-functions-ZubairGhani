8518e1ce80debe1d6ba1c88a1867e223b8d96008
ZubairGhani <ghani.zubairmohammad@gmail.com>
2021-06-16 17:28:31 +0200

    celsius&fahrenheit

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,28 @@
-// define a function multiplyByNineFifths(number)
+// define a 
+function multiplyByNineFifths(number) {
+    return (number * 9 / 5);
+}
 
-    //return number*(9/5)
+//return number*(9/5)
 
-// define a function getFahrenheit(celsius)
+// define a 
+function getFahrenheit(celsius) {
+    return multiplyByNineFifths(celsius) + 32;
+}
 
-    // return multiplyByNineFifths(celsius) + 32;
+console.log(getFahrenheit(18)); // call getFahrenheit();
 
-console.log() // call getFahrenheit();
 
 
 // do the same thing starting from Fahrenheit to Celsius
+function fahrenheit(number1) {
+    return(number1 * 9/5);
+  
+} 
+function getCelsius(Fahre) {
+    return fahrenheit(Fahre) -32 ;
+    
+}
+
+console.log(getCelsius(18)) ;
+    
